799563daee2eb7cc2a0451e6a2429f1fc23eb123
Javad <1998javad@gmail.com>
2024-02-22 17:58:52 +0100

    Fix capitalization of About component filename

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Home.jsx";
 import Menu from "./Menu.jsx";
 import Status from "./Status.jsx";
-import About from "./about.jsx";
+import About from "./About.jsx";
 
 function App() {
   const router = createBrowserRouter([
